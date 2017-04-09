5afc248df743e8cea778ea9de00a443846722a89
DianaDumitru <dianadmaria95@gmail.com>
2017-04-09 14:30:16 +0300

    First commit

---
+++ ACCS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ACCS.Startup))]
+namespace ACCS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
